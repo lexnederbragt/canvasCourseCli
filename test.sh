85338b4c903406e1c121e3858eefc691296a1741
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2019-01-23 14:04:24 +0100

    New filename, updated test script

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-python send_html_to_Canvas.py -i uio -c 5848 -p testing-canvas_api -f api_test.html
+# testing script on UiO canvas instance
+python canvas.py -u https://uio.instructure.com/courses/5848/pages/testing-canvas-api -f api_test.html
