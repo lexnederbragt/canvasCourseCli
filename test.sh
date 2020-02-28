e3562e0c8f02a9023c6c5ce8496161f4afe17368
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2020-02-28 15:27:29 +0100

    Enhanced testing

--- test.sh
+++ test.sh
@@ -1,31 +1,42 @@
-# change URL to the relevant course
-URL=https://uio.instructure.com/courses/4258
+# use as - replacing url with one you have API access to:
+#
+# sh test.sh https://instance.instructure.com/courses/9999
 
-# steps before testing:
+
+# preparational steps before testing:
 # have a page called ${URL}/pages/testing-canvas-api
-# remove page(s) ${URL}/pages/adding-a-page
+# remove any pages whose name is or start with ${URL}/pages/adding-a-page
 # change api_test.html (optional)
 # delete folder ${URL}/test
 
-# this script
-# *
-# * adds a page: ${URL}/pages/adding-a-page
+URL=$1
 
-# testing script on UiO canvas instance
-echo Updating ${URL}/pages/testing-canvas-api
-cvupdate.py update_page -u ${URL}/pages/testing-canvas-api -f api_test.html
-echo
-echo viewing ${URL}/pages/testing-canvas-api
-cvupdate.py view_page -u ${URL}/pages/testing-canvas-api
-echo
-echo creating ${URL}/files/folder/test
-cvupdate.py create_folder -u ${URL}/files/folder/test
-echo
-echo adding file api_test.txt to ${URL}/files/folder/test
-cvupdate.py add_file -u ${URL}/files/folder/test -f api_test.txt
-echo
-echo adding page "Adding a page" from file api_test2.html
-cvupdate.py add_page -u ${URL} -t "Adding a page" -f api_test2.html
-echo
-echo Adding ${URL}/pages/adding-a-page to module 'API test module'
-cvupdate.py add_to_module -u ${URL}/pages/adding-a-page -m 'API test module'
+if [ -z "$URL" ]
+then
+      echo "use as:    test.sh URL"
+else
+
+    echo Updating ${URL}/pages/testing-canvas-api
+    cvupdate.py update_page -u ${URL}/pages/testing-canvas-api -f api_test.html
+    echo
+    echo Viewing ${URL}/pages/testing-canvas-api
+    cvupdate.py view_page -u ${URL}/pages/testing-canvas-api
+    echo
+    echo Creating ${URL}/files/folder/test
+    cvupdate.py create_folder -u ${URL}/files/folder/test
+    echo
+    echo Adding file api_test.txt to ${URL}/files/folder/test
+    cvupdate.py add_file -u ${URL}/files/folder/test -f api_test.txt
+    echo
+    echo Adding and publishing page "Adding a page" from file api_test2.html
+    cvupdate.py add_page -u ${URL} -t "Adding a page" -f api_test2.html -p
+    echo
+    echo Adding ${URL}/pages/adding-a-page to module 'API test module'
+    cvupdate.py add_to_module -u ${URL}/pages/adding-a-page -m 'API test module'
+    echo
+    echo Listing all pages
+    cvupdate.py list_pages -u ${URL}
+    echo
+    echo Listing all files
+    cvupdate.py list_files -u ${URL}
+fi
