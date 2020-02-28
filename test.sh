200440f6a1a47634ff95af07516d94749a48fdf1
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2020-02-28 16:25:33 +0100

    removed need for `.py` in command

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
+# this script can be used with an existing canvas course to test
+# - viewing, updating, and adding a page
+# - create a folder and add a file to it
+# - list all pages and files
+
 # use as - replacing url with one you have API access to:
 #
 # sh test.sh https://instance.instructure.com/courses/9999
-
 
 # preparational steps before testing:
 # have a page called ${URL}/pages/testing-canvas-api
@@ -17,26 +21,26 @@ then
 else
 
     echo Updating ${URL}/pages/testing-canvas-api
-    canvasCourseCli.py update_page -u ${URL}/pages/testing-canvas-api -f api_test.html
+    canvasCourseCli update_page -u ${URL}/pages/testing-canvas-api -f api_test.html
     echo
     echo Viewing ${URL}/pages/testing-canvas-api
-    canvasCourseCli.py view_page -u ${URL}/pages/testing-canvas-api
+    canvasCourseCli view_page -u ${URL}/pages/testing-canvas-api
     echo
     echo Creating ${URL}/files/folder/test
-    canvasCourseCli.py create_folder -u ${URL}/files/folder/test
+    canvasCourseCli create_folder -u ${URL}/files/folder/test
     echo
     echo Adding file api_test.txt to ${URL}/files/folder/test
-    canvasCourseCli.py add_file -u ${URL}/files/folder/test -f api_test.txt
+    canvasCourseCli add_file -u ${URL}/files/folder/test -f api_test.txt
     echo
     echo Adding and publishing page "Adding a page" from file api_test2.html
-    canvasCourseCli.py add_page -u ${URL} -t "Adding a page" -f api_test2.html -p
+    canvasCourseCli add_page -u ${URL} -t "Adding a page" -f api_test2.html -p
     echo
     echo Adding ${URL}/pages/adding-a-page to module 'API test module'
-    canvasCourseCli.py add_to_module -u ${URL}/pages/adding-a-page -m 'API test module'
+    canvasCourseCli add_to_module -u ${URL}/pages/adding-a-page -m 'API test module'
     echo
     echo Listing all pages
-    canvasCourseCli.py list_pages -u ${URL}
+    canvasCourseCli list_pages -u ${URL}
     echo
     echo Listing all files
-    canvasCourseCli.py list_files -u ${URL}
+    canvasCourseCli list_files -u ${URL}
 fi
